59a20566501b63d4b82a1fdcc7fceb1fc50d4272
Jose Corral <angel.corral71@gmail.com>
2015-02-14 01:15:26 -0600

    TableView and Total implemented Need to implement ComboBox for locale changing

--- src/invoiceapp/FXMLDocumentController.java
+++ src/invoiceapp/FXMLDocumentController.java
@@ -15,6 +15,13 @@ import javafx.scene.control.Label;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.property.DoubleProperty;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -23,10 +30,28 @@ import javafx.scene.control.ComboBox;
 public class FXMLDocumentController implements Initializable {
     private final ItemManager im = ItemManager.getInstance();
     private ObservableList<Item> itemList = FXCollections.observableArrayList();
+    //private ObservableList<Item> tableItems = FXCollections.observableList(im.getAllItems());
     
     private Label label;
     @FXML
-    private ComboBox<?> itemsList;
+    private TableView<Item> itemsTable;
+    @FXML
+    private TableColumn<Item, IntegerProperty> itemNumberColumn;
+    
+    @FXML
+    private TableColumn<?, ?> descriptionColumn;
+    @FXML
+    private TableColumn<?, ?> priceColumn;
+    @FXML
+    private TableColumn<?, ?> quantityColumn;
+    @FXML
+    private TableColumn<?, ?> taxColumn;
+    @FXML
+    private TableColumn<?, ?> totalColumn;
+    @FXML
+    private TextField itemsTotal;
+    @FXML
+    private ComboBox<?> listComboBox;
     
     private void handleButtonAction(ActionEvent event) {
        // System.out.println("You clicked me!");
@@ -39,6 +64,7 @@ public class FXMLDocumentController implements Initializable {
         
         buildItems();
         printItems();
+        buildTable();
         
         
     }    
@@ -64,5 +90,25 @@ public class FXMLDocumentController implements Initializable {
                     + " Total = "+ itemList.get(i).total.get());
         }
     }
-    
+    private void buildTable(){
+        
+        ObservableList<Item> tableItems = FXCollections.observableList(im.getAllItems());
+       // System.out.println(tableItems.get(1).price.get());
+        itemNumberColumn.setCellValueFactory(new PropertyValueFactory<>("itemNumber"));
+        descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+        quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+        taxColumn.setCellValueFactory(new PropertyValueFactory<>("tax"));
+        totalColumn.setCellValueFactory(new PropertyValueFactory<>("total"));
+        
+        itemsTable.setItems(tableItems);
+        //Calculate the total of the combined items
+        double sum = 0;
+        for (int i = 0; i < tableItems.size(); i++){
+            sum = sum + tableItems.get(i).total.get();
+        }
+       // System.out.println( ((Number)sum).toString()  );
+        itemsTotal.setText(((Number)sum).toString());
+        
+    }
 }
