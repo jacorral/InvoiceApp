71d9a7b18e29a5ffc7cc03b9422bb5172193b841
Jose Corral <angel.corral71@gmail.com>
2015-02-15 22:30:35 -0600

    Need to implement event handlers for locale changes

--- src/invoiceapp/FXMLDocumentController.java
+++ src/invoiceapp/FXMLDocumentController.java
@@ -9,19 +9,24 @@ import com.DaBandit.invoice.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Locale;
+import java.util.Currency;
+import java.text.NumberFormat;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.DoubleProperty;
 import javafx.scene.control.TextField;
+
 
 /**
  *
@@ -41,19 +46,22 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private TableColumn<?, ?> descriptionColumn;
     @FXML
-    private TableColumn<?, ?> priceColumn;
+    private TableColumn<Item, Double> priceColumn;
     @FXML
     private TableColumn<?, ?> quantityColumn;
     @FXML
     private TableColumn<?, ?> taxColumn;
     @FXML
-    private TableColumn<?, ?> totalColumn;
+    private TableColumn<Item, Double> totalColumn;
     @FXML
     private TextField itemsTotal;
     @FXML
     private ComboBox<String> listComboBox;
     
     private  ResourceBundle resources;
+    private Currency currentCurrency;
+    private NumberFormat currencyFormatter;
+    
     
     private void handleButtonAction(ActionEvent event) {
        // System.out.println("You clicked me!");
@@ -65,8 +73,13 @@ public class FXMLDocumentController implements Initializable {
     //@FXML
     //public void initialize( ){
        
-        Locale locale = Locale.getDefault();
-        resources = ResourceBundle.getBundle("properties.Bundle", locale);
+        //Locale locale = Locale.getDefault();
+        Locale locale = new Locale("zh","CN");
+        resources = ResourceBundle.getBundle("properties.Bundle_es_MX", locale);
+        
+        //Currency formatter
+        currentCurrency = Currency.getInstance(locale);
+        currencyFormatter = NumberFormat.getCurrencyInstance(locale);
         
         //resources = rb;
         setTable();
@@ -78,7 +91,7 @@ public class FXMLDocumentController implements Initializable {
         //fxmlLoader.setResources(resources);
         buildItems();
         printItems();
-        buildTable();
+        buildTable(locale);
         listComboBox.getItems().addAll("Chicago", "Mexico", "Brazil");
        // listComboBox.setValue(null);
         
@@ -108,7 +121,14 @@ public class FXMLDocumentController implements Initializable {
                     + " Total = "+ itemList.get(i).total.get());
         }
     }
-    private void buildTable(){
+    private void buildTable(Locale locale){
+        // Locale locale = new Locale("zh", "CN");
+        //resources = ResourceBundle.getBundle("properties.Bundle", locale);
+        
+        //Currency formatter
+        currentCurrency = Currency.getInstance(locale);
+        currencyFormatter = NumberFormat.getCurrencyInstance(locale);
+        
         
         
         ObservableList<Item> tableItems = FXCollections.observableList(im.getAllItems());
@@ -116,9 +136,43 @@ public class FXMLDocumentController implements Initializable {
         itemNumberColumn.setCellValueFactory(new PropertyValueFactory<>("itemNumber"));
         descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
         quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        
+        
         priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+        priceColumn.setCellFactory(column ->{
+            return new TableCell<Item, Double>(){
+                @Override
+                protected void updateItem(Double num, boolean empty){
+                    super.updateItem(num, empty);
+                    if (num == null || empty){
+                        setText(null);
+                        setStyle("");
+                    }else{
+                        setText(currencyFormatter.format(num));
+                    }
+                }
+                
+            };
+        });
+        
+        
         taxColumn.setCellValueFactory(new PropertyValueFactory<>("tax"));
         totalColumn.setCellValueFactory(new PropertyValueFactory<>("total"));
+        totalColumn.setCellFactory(column ->{
+            return new TableCell<Item, Double>(){
+                @Override
+                protected void updateItem(Double num, boolean empty){
+                    super.updateItem(num, empty);
+                    if (num == null || empty){
+                        setText(null);
+                        setStyle("");
+                    }else{
+                        setText(currencyFormatter.format(num));
+                    }
+                }
+                
+            };
+        });
         
         itemsTable.setItems(tableItems);
         //Calculate the total of the combined items
@@ -127,7 +181,8 @@ public class FXMLDocumentController implements Initializable {
             sum = sum + tableItems.get(i).total.get();
         }
        // System.out.println( ((Number)sum).toString()  );
-        itemsTotal.setText(((Number)sum).toString());
+        
+        itemsTotal.setText(currencyFormatter.format(sum));
         
     }
     
@@ -135,6 +190,7 @@ public class FXMLDocumentController implements Initializable {
         //Update headings
      //  Locale locale = new Locale("es", "MX");
      //  resources = ResourceBundle.getBundle("properties.Bundle_es_MX",locale);
+       // currentCurrency 
         
         itemNumberColumn.setText(resources.getString("item"));
         descriptionColumn.setText(resources.getString("description"));
